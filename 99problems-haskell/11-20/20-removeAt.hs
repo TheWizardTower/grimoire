126841c57c6e128a6013ffd92522864c4b10055e
Adam McCullough <merlinfmct87@gmail.com>
2015-08-24 21:58:58 -0700

    Solution for problem 20!

---
+++ 99problems-haskell/11-20/20-removeAt.hs
@@ -0,0 +1,9 @@
+module RemoveAt where
+
+removeAt :: Int -> [a] -> (a, [a])
+removeAt _      [] = error "Cannot remove element from empty list."
+removeAt 0     (x:xs) = (x, xs)
+removeAt index (x:xs) = (element, array)
+               where
+                 element = head (drop (index - 1) (x:xs))
+                 array = (take (index - 1) (x:xs)) ++ (drop (index) (x:xs))
