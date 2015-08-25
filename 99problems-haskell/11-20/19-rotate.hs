b1ecea03f901285916b9440b6ac1ade35b2964de
Adam McCullough <merlinfmct87@gmail.com>
2015-08-24 21:47:37 -0700

    Solution for problem 19. Amazing what playing around in the interpreter can let you uncover.

---
+++ 99problems-haskell/11-20/19-rotate.hs
@@ -0,0 +1,12 @@
+module Rotate where
+
+rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate (x:xs) 0 = (x:xs)
+rotate (x:xs) pivot | pivot > 0 = p2 ++ p1
+                    | pivot < 0 = n2 ++ n1
+                      where
+                        p1 = take pivot (x:xs)
+                        p2 = drop pivot (x:xs)
+                        n1 = take (pivot + (length (x:xs))) (x:xs)
+                        n2 = drop (pivot + (length (x:xs))) (x:xs)
