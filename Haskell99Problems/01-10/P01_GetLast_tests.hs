96e954df3655dabb74c178e3adde6b214e5035bb
Adam McCullough <merlinfmct87@gmail.com>
2015-11-29 02:38:50 -0800

    More cleanup work. This is close to working.

--- Haskell99Problems/01-10/P01_GetLast_tests.hs
+++ Haskell99Problems/01-10/P01_GetLast_tests.hs
@@ -1,4 +1,4 @@
--- import GetLast
+import           P01_GetLast
 import           Test.QuickCheck
 
 prop_equivalent_to_last = forAll (listOf1 arbitrary) $ (\s -> (getLast s) == (last s))
---
+++ Haskell99Problems/app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import Haskell99Problems
+import Test.QuickCheck
+
+main :: IO ()
+main = quickCheckAll
