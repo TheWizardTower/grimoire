9ec203f48aeef9b623686bc09a00d14719ee0e8c
Adam McCullough <merlinfmct87@gmail.com>
2015-11-29 04:16:32 -0800

    This way seems awfully boiler-plate-ish. I'll figure out something better later.

--- Haskell99Problems/01-10/P01_GetLast_Tests.hs
+++ Haskell99Problems/01-10/P01_GetLast_Tests.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module P01_GetLast_Tests where
 
 import           P01_GetLast
 import           Test.QuickCheck
 
 prop_equivalent_to_last = forAll (listOf1 arbitrary) $ ( (\s -> (getLast s) == (last s)) :: [Int] -> Bool)
+
+
+
+return []
+runTests = $quickCheckAll
--- Haskell99Problems/app/Main.hs
+++ Haskell99Problems/app/Main.hs
@@ -3,9 +3,11 @@
 module Main where
 
 -- import Haskell99Problems
+import           P01_GetLast_Tests
 import           Test.QuickCheck
 import           Test.QuickCheck.All
 
 return []
-
-main = $(quickCheckAll)
+main = do {
+  P01_GetLast_Tests.runTests
+}
