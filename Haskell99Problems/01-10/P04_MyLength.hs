09e4e3a6115a52c872bf27ca6ef8ecc68651401c
Adam McCullough <merlinfmct87@gmail.com>
2015-11-29 03:00:13 -0800

    Needs an explicit type signature.

--- Haskell99Problems/01-10/P04_MyLength.hs
+++ Haskell99Problems/01-10/P04_MyLength.hs
@@ -9,4 +9,7 @@ myLengthAcc :: [a] -> Int -> Int
 myLengthAcc [] acc     = acc
 myLengthAcc (x:xs) acc = myLengthAcc xs (acc + 1)
 
+
+-- This fails with a type exception. Either fix or remove.
+myAltLength :: (Num b, Foldable t) => t a -> b
 myAltLength = foldl' (\acc x -> acc + 1) 0
