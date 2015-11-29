ea4ceb913b9e700259d768718dc40850473948ec
Adam McCullough <merlinfmct87@gmail.com>
2015-11-29 03:59:04 -0800

    Fix to build file and type signature details.

--- Haskell99Problems/01-10/P01_GetLast_Tests.hs
+++ Haskell99Problems/01-10/P01_GetLast_Tests.hs
@@ -1,6 +1,6 @@
-module P01_GetLast_Test where
+module P01_GetLast_Tests where
 
 import           P01_GetLast
 import           Test.QuickCheck
 
-prop_equivalent_to_last = forAll (listOf1 arbitrary) $ (\s -> (getLast s) == (last s))
+prop_equivalent_to_last = forAll (listOf1 arbitrary) $ ( (\s -> (getLast s) == (last s)) :: [Int] -> Bool)
