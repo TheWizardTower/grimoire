83d578f78c69ee6bcd5e43213449707055a34e3b
Adam McCullough <merlinfmct87@gmail.com>
2015-11-29 03:02:00 -0800

    Remove stale comment.

--- Haskell99Problems/01-10/P04_MyLength.hs
+++ Haskell99Problems/01-10/P04_MyLength.hs
@@ -10,6 +10,5 @@ myLengthAcc [] acc     = acc
 myLengthAcc (x:xs) acc = myLengthAcc xs (acc + 1)
 
 
--- This fails with a type exception. Either fix or remove.
 myAltLength :: (Num b, Foldable t) => t a -> b
 myAltLength = foldl' (\acc x -> acc + 1) 0
