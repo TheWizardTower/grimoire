79f069f202a7ddb6bea2f380b6f5e1283a5151f2
Adam McCullough <merlinfmct87@gmail.com>
2015-11-30 01:08:42 -0800

    Basic tests for problem 02.

--- Haskell99Problems/app/Main.hs
+++ Haskell99Problems/app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 -- import Haskell99Problems
 import           P01_GetLast_Tests
+import           P02_ButLast_Tests
 import           P05_MyReverse_Tests
 import           Test.QuickCheck
 import           Test.QuickCheck.All
@@ -11,4 +12,5 @@ import           Test.QuickCheck.All
 return []
 main = do
   P01_GetLast_Tests.runTests
+  P02_ButLast_Tests.runTests
   P05_MyReverse_Tests.runTests
