2b97f8c2d5188951f1818eeb07a6f11192ccb0c4
Adam McCullough <merlinfmct87@gmail.com>
2015-11-30 00:26:25 -0800

    Adding HUnit support, and some tests using it.

--- Haskell99Problems/app/Main.hs
+++ Haskell99Problems/app/Main.hs
@@ -4,10 +4,11 @@ module Main where
 
 -- import Haskell99Problems
 import           P01_GetLast_Tests
+import           P05_MyReverse_Tests
 import           Test.QuickCheck
 import           Test.QuickCheck.All
 
 return []
-main = do {
+main = do
   P01_GetLast_Tests.runTests
-}
+  P05_MyReverse_Tests.runTests
