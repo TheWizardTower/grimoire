fb78cb5aeebc208bf62be0351df3d286582ca0a3
Adam McCullough <merlinfmct87@gmail.com>
2015-12-24 20:45:04 -0800

    Hello, typo!

--- Haskell99Problems/21-30/P21_InsertAt.hs
+++ Haskell99Problems/21-30/P21_InsertAt.hs
@@ -2,7 +2,7 @@ module P21_InsertAt where
 
 insertAt ::  a -> [a] -> Int -> [a]
 insertAt _ [] _            = error "Cannot insert element into empty list."
-insertAt elem  (x:xs) 0    = (elem:x:xs
+insertAt elem  (x:xs) 0    = (elem:x:xs)
 insertAt elem (x:xs) index = p1 ++ [elem] ++ p2
               where
                 p1 = take (index - 1) (x:xs)
