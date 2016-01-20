35d4dc87115de5171ef4f528b351376311469b5a
Adam McCullough <merlinfmct87@gmail.com>
2016-01-19 22:30:55 -0800

    Seems to pass initial manual tests.

---
+++ Haskell99Problems/21-30/P26_Combinations.hs
@@ -0,0 +1,12 @@
+module P26_Combinations where
+
+
+combinatorial :: Int -> [a] -> [[a]]
+combinatorial _ []     = [[]]
+combinatorial 0  _     = [[]]
+combinatorial k  x     | k > length x = error "Cannot choose more elements than in the list."
+combinatorial _ [x]    = [[x]]
+combinatorial k (x:xs) | k <  length (x:xs)  = map (x:) (combinatorial (k - 1) xs) ++ combinatorial k xs
+--                       | k == length (x:xs)  = [[x]] ++ combinatorial (k -1) xs
+                       | k == length (x:xs)  = [x:xs]
+                       | k >  length (x:xs)  = error "Cannot choose more elements than in the list."
