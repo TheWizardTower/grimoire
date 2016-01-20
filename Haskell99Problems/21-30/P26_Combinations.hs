c5bd20ba737dc37b4e84f3d9ca761d26f8cd0fda
Adam McCullough <merlinfmct87@gmail.com>
2016-01-19 22:43:27 -0800

    Delete a comment and add to project.

--- Haskell99Problems/21-30/P26_Combinations.hs
+++ Haskell99Problems/21-30/P26_Combinations.hs
@@ -7,6 +7,5 @@ combinatorial 0  _     = [[]]
 combinatorial k  x     | k > length x = error "Cannot choose more elements than in the list."
 combinatorial _ [x]    = [[x]]
 combinatorial k (x:xs) | k <  length (x:xs)  = map (x:) (combinatorial (k - 1) xs) ++ combinatorial k xs
---                       | k == length (x:xs)  = [[x]] ++ combinatorial (k -1) xs
                        | k == length (x:xs)  = [x:xs]
                        | k >  length (x:xs)  = error "Cannot choose more elements than in the list."
